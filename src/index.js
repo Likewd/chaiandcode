7dfb5269c4ed7f4a1bbbd13d258769179293eaf3
likewd <seeheremeu@gmail.com>
2024-07-26 03:23:01 -0700

    utils code secction

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import dotenv  from "dotenv"
 
 // import express from "express";
 import contactDB from "./db/index.js";
+import app from "./app.js";
 // const app = express()
     
 dotenv.config({
     path:'./env'
 })
 
-contactDB()
+contactDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+    console.log(`server is running at port ${process.env.PORT}`)
+})
+
+ }).catch(error => {
+    console.log(`Error in DataBase Connection failed ${process.env.PORT}`)
+})
